ae97f71d991cbf150eb617729d3da668d9130734
Sayali Pathak <95687460+sayalipathak7@users.noreply.github.com>
2025-06-18 23:05:35 -0500

    Time: 1 ms (26.35%), Space: 44.2 MB (94.06%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -14,30 +14,17 @@
  * }
  */
 class Solution {
-    // public boolean isBalanced(TreeNode root) {
-    //     if(root==null) return true;
-    //     if(Math.abs(ht(root.left)-ht(root.right))>1) return false;
-    //     return (isBalanced(root.left) && isBalanced(root.right));
-    // }
-    // public int ht(TreeNode root)
-    // {
-    //     if(root==null)return 0;
-    //     return Math.max(ht(root.left),ht(root.right))+1;
-    // }
-
-     public boolean isBalanced(TreeNode root) {
-        if(root==null) return true;
-        if(ht(root)==-1) return false;
-        return true;
+    public boolean isBalanced(TreeNode root) {
+        if(root==null)return true;
+        if(Math.abs(ht(root.left)-ht(root.right))>1)return false;
+        return (isBalanced(root.left) && isBalanced(root.right));
+        
     }
     public int ht(TreeNode root)
     {
         if(root==null)return 0;
-        int lh=ht(root.left);
-        if(lh==-1)return -1;
-        int rh=ht(root.right);
-        if(rh==-1)return -1;
-        if(Math.abs(lh-rh)>1) return -1;
-        return Math.max(lh,rh)+1;
+        int l=ht(root.left);
+        int r=ht(root.right);
+        return Math.max(l,r)+1;
     }
 }
