df6cb6a5d8d0f742ebda0677ac1c52ca6918d2eb
Sayali Pathak <95687460+sayalipathak7@users.noreply.github.com>
2025-08-09 23:45:11 -0500

    Time: 2 ms (84.26%), Space: 52.1 MB (87.26%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.java
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.java
@@ -16,20 +16,22 @@
 class Solution {
     public int goodNodes(TreeNode root) {
         if(root==null)return 0;
-        int[] cnt=new int[1];
-        goodNodes(root,root.val,cnt);
-        return cnt[0];
+        int [] a=new int[1];
+        a[0]=0;
+        goodNodes(root,a,Integer.MIN_VALUE);
+        return a[0];
+
         
     }
-    public void goodNodes(TreeNode root,Integer max,int[] cnt) {
+    public void goodNodes(TreeNode root,int[] a,int max)
+    {
         if(root==null)return;
-        if(root.val>=max)
-        {
-            cnt[0]=cnt[0]+1;
-            max=root.val;
-        }
-        goodNodes(root.left,max,cnt);
-        goodNodes(root.right,max,cnt);
-    
+        max=Math.max(root.val,max);
+        if(root.val>=max) a[0]=a[0]+1;
+        goodNodes(root.left,a,max);
+        goodNodes(root.right,a,max);
+
+
+
     }
 }
