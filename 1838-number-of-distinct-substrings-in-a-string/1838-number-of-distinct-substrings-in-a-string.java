e30b8230ecb5c8c0911a6d0d54c627173470aa6f
Sayali Pathak <95687460+sayalipathak7@users.noreply.github.com>
2025-08-17 21:19:12 -0500

    Time: 666 ms (14.41%), Space: 115.4 MB (23.42%) - LeetHub

--- 1838-number-of-distinct-substrings-in-a-string/1838-number-of-distinct-substrings-in-a-string.java
+++ 1838-number-of-distinct-substrings-in-a-string/1838-number-of-distinct-substrings-in-a-string.java
@@ -3,11 +3,10 @@ class Solution {
         HashSet<String> h=new HashSet<>();
         for(int i=0;i<s.length();i++)
         {
-            String str="";
             for(int j=i;j<s.length();j++)
             {
-                str=str+s.charAt(j);
-                h.add(str);
+
+                h.add(s.substring(i,j+1));
             }
         }
         return h.size();
