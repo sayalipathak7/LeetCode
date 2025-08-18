655b6aa3444709b1ebb0ac64b3aeaf1ab707ef30
Sayali Pathak <95687460+sayalipathak7@users.noreply.github.com>
2025-08-17 21:55:15 -0500

    Time: 46 ms (77.48%), Space: 81.5 MB (82.88%) - LeetHub

--- 1838-number-of-distinct-substrings-in-a-string/1838-number-of-distinct-substrings-in-a-string.java
+++ 1838-number-of-distinct-substrings-in-a-string/1838-number-of-distinct-substrings-in-a-string.java
@@ -1,15 +1,41 @@
+class Node
+{
+    Node[] children;
+    boolean eow;
+    Node()
+    {
+        children=new Node[26];
+        eow=false;
+    }
+}
 class Solution {
+    Node root=new Node();
+    int count=0;
     public int countDistinct(String s) {
-        HashSet<String> h=new HashSet<>();
+        ArrayList<String> suffixList= new ArrayList<>();
         for(int i=0;i<s.length();i++)
         {
-            for(int j=i;j<s.length();j++)
-            {
-
-                h.add(s.substring(i,j+1));
-            }
+            suffixList.add(s.substring(i,s.length()));
         }
-        return h.size();
+        System.out.println(suffixList.toString());
+        for(String word:suffixList) insert(word);
+        return count;
         
+    }
+    public void insert(String word)
+    {
+        Node curr=root;
+        for(int i=0;i<word.length();i++)
+        {
+            int idx=word.charAt(i)-'a';
+            if(curr.children[idx]==null)
+            {
+                curr.children[idx]=new Node();
+                count++;
+            }
+            if(i==word.length()-1)curr.children[idx].eow=true;
+            curr=curr.children[idx];
+        }
+
     }
 }
