0b5320c7f75b46f9cd0055c9f48a1a4bb8b5fc79
Sayali Pathak <95687460+sayalipathak7@users.noreply.github.com>
2025-08-26 10:57:38 -0500

    Time: 0 ms (100%), Space: 46.3 MB (67.14%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,29 +1,25 @@
 class Solution {
     public int trap(int[] height) {
-        int[] prefix_ht=new int[height.length];
-        int[] sufix_ht=new int[height.length];
-        prefix_ht[0]=0;
-        sufix_ht[height.length-1]=0;
-        int ttl=0;
-        for(int i=1;i<height.length;i++)
+        int l=0,r=height.length-1;
+        int lm=0,rm=0;
+        int ans=0;
+        while(l<r)
         {
-            prefix_ht[i]=Math.max(height[i-1],prefix_ht[i-1]);
-        }
-        for(int i=height.length-2;i>=0;i--)
-        {
-            sufix_ht[i]=Math.max(height[i+1],sufix_ht[i+1]);
-        }
-        for(int i=0;i<height.length;i++)
-        {
-            int water_trap=Math.min(prefix_ht[i],sufix_ht[i])-height[i];
-            if(water_trap>0)
+            lm=Math.max(lm,height[l]);
+            rm=Math.max(rm,height[r]);
+            if(lm<rm)
             {
-                ttl=ttl+water_trap;
+                ans=ans+lm-height[l];
+                l++;
 
             }
-        }
-        return ttl;
+            else
+            {
+                ans=ans+rm-height[r];
+                r--;
+            }
 
-        
+        }
+        return ans;
     }
 }
