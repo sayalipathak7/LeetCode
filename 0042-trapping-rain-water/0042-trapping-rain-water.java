160f2a8c15ceb4b48ccc6429efbdf5747d400c03
Sayali Pathak <95687460+sayalipathak7@users.noreply.github.com>
2025-08-26 10:18:35 -0500

    Time: 1 ms (61.24%), Space: 46 MB (89.08%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,51 +1,28 @@
 class Solution {
     public int trap(int[] height) {
-
-        int left_max=0;
-        int right_max=0;
-        int l=0,r=height.length-1,ans=0;
-        while(l<r)
+        int[] prefix_ht=new int[height.length];
+        int[] sufix_ht=new int[height.length];
+        prefix_ht[0]=0;
+        sufix_ht[height.length-1]=0;
+        int ttl=0;
+        for(int i=1;i<height.length;i++)
         {
-            left_max=Math.max(left_max,height[l]);
-            right_max=Math.max(right_max,height[r]);
-            if(left_max<right_max)
-            {
-                ans=ans+left_max-height[l];
-                l++;
-            }
-            else
-            {
-                ans=ans+right_max-height[r];
-                r--;
-            }
-
-
+            prefix_ht[i]=Math.max(height[i-1],prefix_ht[i-1]);
         }
-        return ans;
-        // int [] prefix=new int[height.length];
-        // int [] suffix=new int[height.length];
-        // prefix[0]=0;
-        // suffix[height.length-1]=0;
-        // int ttl=0;
-       
+        for(int i=height.length-2;i>=0;i--)
+        {
+            sufix_ht[i]=Math.max(height[i+1],sufix_ht[i+1]);
+        }
+        for(int i=0;i<height.length;i++)
+        {
+            int water_trap=Math.min(prefix_ht[i],sufix_ht[i])-height[i];
+            if(water_trap>0)
+            {
+                ttl=ttl+water_trap;
 
-        // for(int i=1;i<height.length;i++)
-        // {
-        //     prefix[i]=Math.max(height[i-1],prefix[i-1]);
-        // }
-        // for(int i=height.length-2;i>=0;i--)
-        // {
-        //     suffix[i]=Math.max(height[i+1],suffix[i+1]);
-        // }
-        // for(int i=0;i<height.length;i++)
-        // {
-        //     int waterTrapped=Math.min(prefix[i],suffix[i])-height[i];
-        //     if(waterTrapped>0)
-        //     ttl=ttl+waterTrapped;
-
-        // }
-        // return ttl;
-
+            }
+        }
+        return ttl;
 
         
     }
