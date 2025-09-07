ba1b1f3e6d117e253d959b5bc49c5ef95a1a40e3
Sayali Pathak <95687460+sayalipathak7@users.noreply.github.com>
2025-09-07 07:40:43 -0500

    Time: 2 ms (64.22%), Space: 44.7 MB (46.08%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -2,31 +2,28 @@ class Solution {
     public boolean isValidSudoku(char[][] board) {
         HashSet<Character> [] rows=new HashSet[9];
         HashSet<Character> [] cols=new HashSet[9];
-        HashSet<Character> [] sq=new HashSet[9];
+        HashSet<Character> [] sqs=new HashSet[9];
         for(int i=0;i<9;i++)
         {
             rows[i]=new HashSet<>();
             cols[i]=new HashSet<>();
-            sq[i]=new HashSet<>();
+            sqs[i]=new HashSet<>();
         }
-        for(int i=0;i<board.length;i++)
+        for(int r=0;r<9;r++)
         {
-            for(int j=0;j<board[0].length;j++)
+            for(int c=0;c<9;c++)
             {
-                char num=board[i][j];
-                if(num=='.')continue;
-                //check row
-                if(rows[i].contains(num)) return false;
-                else rows[i].add(num);
+                if(board[r][c]=='.')continue;
 
-                // check col
-                if(cols[j].contains(num)) return false;
-                else cols[j].add(num);
+                if(rows[r].contains(board[r][c]))return false;
+                rows[r].add(board[r][c]);
 
-                //check sq
-                int s=(i/3)*3+(j/3);
-                if(sq[s].contains(num)) return false;
-                else sq[s].add(num);
+                if(cols[c].contains(board[r][c])) return false;
+                cols[c].add(board[r][c]);
+
+                int sq= r/3*3+c/3;
+                if(sqs[sq].contains(board[r][c]))return false;
+                sqs[sq].add(board[r][c]);
 
             }
         }
