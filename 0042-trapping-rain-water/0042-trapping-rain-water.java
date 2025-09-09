d1803ddf9c8491e1066b6f82cf4bccfff7cca980
Sayali Pathak <95687460+sayalipathak7@users.noreply.github.com>
2025-09-08 22:24:14 -0500

    Time: 1 ms (61.23%), Space: 46.2 MB (74.32%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,26 +1,28 @@
 class Solution {
     public int trap(int[] height) {
-        int[] p=new int[height.length];
-        int[] s=new int[height.length];
+        int [] p=new int[height.length];
+        int [] s=new int[height.length];
         p[0]=0;
         s[height.length-1]=0;
+        int total=0;
         for(int i=1;i<height.length;i++)
         {
-            p[i]=Math.max(p[i-1],height[i-1]);
+            p[i]=Math.max(height[i-1],p[i-1]);
+
         }
         for(int i=height.length-2;i>=0;i--)
         {
-            s[i]=Math.max(s[i+1],height[i+1]);
+            s[i]=Math.max(height[i+1],s[i+1]);
         }
-        int cnt=0;
         for(int i=0;i<height.length;i++)
         {
-            int ttl=Math.min(p[i],s[i])-height[i];
-            if(ttl>0)
-            cnt=ttl+cnt;
+            if(Math.min(p[i],s[i])-height[i]>0)
+            {
+                total=total+Math.min(p[i],s[i])-height[i];
+            }
 
         }
-        return cnt;
+        return total;
         
     }
 }
