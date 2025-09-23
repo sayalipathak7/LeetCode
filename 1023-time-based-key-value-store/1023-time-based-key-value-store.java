dd58451387a2bca97d407274b3f10b8b97311f29
Sayali Pathak <95687460+sayalipathak7@users.noreply.github.com>
2025-09-23 18:05:37 -0500

    Time: 143 ms (53.55%), Space: 119.7 MB (58.07%) - LeetHub

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.java
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.java
@@ -1,46 +1,53 @@
 class TimeMap {
-    
-    HashMap<String,ArrayList<Pair<Integer,String>>> hm=new HashMap<>();
+    HashMap<String,ArrayList<Pair<String,Integer>>> hm;
 
     public TimeMap() {
+        hm=new HashMap<>();
         
     }
     
     public void set(String key, String value, int timestamp) {
-        Pair<Integer,String> p=new Pair<>(timestamp,value);
         if(!hm.containsKey(key))
         {
-            ArrayList<Pair<Integer,String>> q=new ArrayList<>();
-            hm.put(key,q);
-           
+            hm.put(key,new ArrayList<Pair<String,Integer>>());
         }
+        Pair<String,Integer> p=new Pair<>(value,timestamp);
         hm.get(key).add(p);
         
     }
     
     public String get(String key, int timestamp) {
-         if(!hm.containsKey(key)) return "";
-        
-      
-            ArrayList<Pair<Integer,String>>al=hm.get(key);
-            int l=0,r=al.size()-1;
-            String res="";
+        if(hm.containsKey(key))
+        {
+            ArrayList<Pair<String,Integer>> al=hm.get(key);
+            int l=0, r=al.size()-1;
+            String val="";
+            int t=0;
             while(l<=r)
             {
                 int mid=l+(r-l)/2;
-                if(al.get(mid).getKey()<=timestamp) 
-               
+                Pair<String,Integer> p1=al.get(mid);
+                int tp=p1.getValue();
+                
+                if(tp<=timestamp) 
                 {
-                    res= al.get(mid).getValue();
+                    if(tp>t)
+                    {
+                        t=tp;
+                        val=p1.getKey();;
+                    } 
                     l=mid+1;
-                }
+                } 
                 else
                 {
                     r=mid-1;
                 }
+
             }
-       
-        return res;
+            return val;
+
+        }
+        else return "";
         
     }
 }
