81d7d60763033fe546f32b3249e4b0206551d3e7
Sayali Pathak <95687460+sayalipathak7@users.noreply.github.com>
2025-11-14 08:58:06 -0600

    Time: 1 ms (42.94%), Space: 46.7 MB (11.71%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -3,14 +3,15 @@ class Solution {
         if(color==image[sr][sc]) return image;
         int n_r=image.length;
         int n_c=image[0].length;
-        int [][]res=new int[n_r][n_c];
-        for(int i=0;i<n_r;i++)
-        {
-            res[i]=image[i].clone();
-        }
+        //int [][]res=new int[n_r][n_c];
+        // for(int i=0;i<n_r;i++)
+        // {
+        //     res[i]=image[i].clone();
+        // }
         int [][] t={{0,1},{0,-1},{1,0},{-1,0}};
         Queue<int[]>q=new LinkedList<>();
-        res[sr][sc]=color;
+        int orignalColor=image[sr][sc];
+        image[sr][sc]=color;
         q.offer(new int[]{sr,sc});
         while(!q.isEmpty())
         {
@@ -21,18 +22,18 @@ class Solution {
             {
                 int row=r+t[i][0];
                 int col=c+t[i][1];
-                if(row>=0 && row<n_r && col>=0 && col<n_c && res[row][col]!=color) 
+                if(row>=0 && row<n_r && col>=0 && col<n_c && image[row][col]!=color) 
                 {
-                    if(image[row][col]==image[sr][sc])
+                    if(image[row][col]==orignalColor)
                     {
-                        res[row][col]=color;
+                        image[row][col]=color;
                         q.offer(new int[]{row,col});
 
                     }
                 }
             }
         }
-        return res;
+        return image;
         
     }
 }
