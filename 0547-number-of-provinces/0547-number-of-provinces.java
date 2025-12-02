dbdcb8ec0a2d3e8405871f1031db65e1f7f23842
Sayali Pathak <95687460+sayalipathak7@users.noreply.github.com>
2025-12-02 10:23:22 -0600

    Time: 2 ms (48.4%), Space: 47 MB (84.65%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,31 +1,61 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int cnt=0;
-        boolean[] v=new boolean[isConnected.length];
-        for(int i=0;i<isConnected.length;i++)
+        int n=isConnected.length;
+        int[] par=new int[n];
+        int[] size=new int[n];
+        for(int i=0;i<n;i++)
         {
-            if(!v[i])
+            par[i]=i;
+        }
+        for(int i=0;i<n;i++)
+        {
+            size[i]=1;
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<isConnected[0].length;j++)
             {
-                cnt++;
-                dfs(i,v,isConnected);
-
+                if(isConnected[i][j]==1)
+                {
+                    unionfind(i,j,par,size);
+                }
             }
-
+        }
+       int cnt=0;
+        for(int i=0;i<n;i++)
+        {
+           if(i==par[i])
+           cnt++;
         }
         return cnt;
         
     }
-    public void dfs(int node, boolean[] v, int[][] isConnected)
+    public void unionfind(int u,int v,int[] par,int[] size)
     {
-        v[node]=true;
-        for(int i=0;i<isConnected.length;i++)
+        int ulU=ulParent(u,par);
+        int ulV=ulParent(v,par);
+        if(ulU==ulV)return;
+        int ulUSize=size[ulU];
+        int ulVSize=size[ulV];
+        if(ulUSize<ulVSize)
         {
-            if(!v[i] && isConnected[node][i]==1)
-            {
-                v[i]=true;
-                dfs(i,v,isConnected);
-            }
+            par[ulU]=ulV;
+            size[ulV]=size[ulV]+size[ulU];
         }
+        else
+        {
+            par[ulV]=ulU;
+            size[ulU]=size[ulU]+size[ulV];
+        }
+
+    }
+    public int ulParent(int x,int[] par)
+    {
+        if(par[x]==x)return x;
+        int p=ulParent(par[x],par);
+        par[x]=p;
+        return par[x];
+
 
     }
 }
