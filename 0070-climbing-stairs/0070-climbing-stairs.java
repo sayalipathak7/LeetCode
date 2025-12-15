fde70e5ffa615ec408388152dcc4a4c9c1a02a77
Sayali Pathak <95687460+sayalipathak7@users.noreply.github.com>
2025-12-15 11:52:16 -0600

    Time: 0 ms (100%), Space: 42.2 MB (6.51%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,24 +1,29 @@
 class Solution {
-    public int climbStairs(int n) {
-        // if(n==1 || n==2) return n;
-        // int[] dp=new int[n];
-        // dp[0]=1;
-        // dp[1]=2;
-        // for(int i=2;i<n;i++)
-        // {
-        //     dp[i]=dp[i-1]+dp[i-2];
-
-        // }
-        // return dp[n-1];
-        if(n <= 2) return n;
-        int a = 1, b = 2;
-        for(int i=3; i<=n; i++){
-            int temp = a + b;
-            a = b;
-            b = temp;
+    // with recursion - Time Limit Exceeded
+    // public int climbStairs(int n) {
+    //     if(n==1 || n==2) return n;
+    //     return climbStairs(n-1)+climbStairs(n-2);
+        
+    // }
+    
+    //==================================================================================================================================================
+    
+    // Top down - recursion + memoization
+      public int climbStairs(int n) {
+        if(n==1 || n==2) return n;
+        int [] res=new int[n];
+        res[0]=1;
+        res[1]=2;
+        return climbStairs(n,res);
+      }
+      public int climbStairs(int n,int [] res) {
+       // if(n==1 || n==2) return n;
+        if(res[n-1]!=0)
+        {
+            return res[n-1];
         }
-        return b;
-       
-    }
+        res[n-1]=climbStairs(n-1,res)+climbStairs(n-2,res);
+        return res[n-1];
 
+      }
 }
