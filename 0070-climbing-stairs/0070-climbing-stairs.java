46f429277945dff61574cb17c72f50124207c612
Sayali Pathak <95687460+sayalipathak7@users.noreply.github.com>
2025-12-15 13:22:27 -0600

    Time: 0 ms (100%), Space: 42 MB (49.5%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,5 +1,5 @@
 class Solution {
-    // with recursion - Time Limit Exceeded
+    // with recursion - Time Limit Exceeded tc - 2 ^n sc - n
     // public int climbStairs(int n) {
     //     if(n==1 || n==2) return n;
     //     return climbStairs(n-1)+climbStairs(n-2);
@@ -8,22 +8,33 @@ class Solution {
     
     //==================================================================================================================================================
     
-    // Top down - recursion + memoization
-      public int climbStairs(int n) {
+    // Top down - recursion + memoization  tc -n sc- n
+    //   public int climbStairs(int n) {
+    //     if(n==1 || n==2) return n;
+    //     int [] res=new int[n];
+    //     res[0]=1;
+    //     res[1]=2;
+    //     return climbStairs(n,res);
+    //   }
+    //   public int climbStairs(int n,int [] res) {
+    //    // if(n==1 || n==2) return n;
+    //     if(res[n-1]!=0)
+    //     {
+    //         return res[n-1];
+    //     }
+    //     res[n-1]=climbStairs(n-1,res)+climbStairs(n-2,res);
+    //     return res[n-1];
+
+    //   }
+    public int climbStairs(int n) {
         if(n==1 || n==2) return n;
         int [] res=new int[n];
         res[0]=1;
         res[1]=2;
-        return climbStairs(n,res);
-      }
-      public int climbStairs(int n,int [] res) {
-       // if(n==1 || n==2) return n;
-        if(res[n-1]!=0)
+        for(int i=3;i<=n;i++)
         {
-            return res[n-1];
+            res[i-1]=res[i-2]+res[i-3];
         }
-        res[n-1]=climbStairs(n-1,res)+climbStairs(n-2,res);
         return res[n-1];
-
-      }
+       }
 }
